35bfbc1c894ad64fe9fadca3f0a846b2a1214b9d
Bianca Fu <0509biancafu@gmail.com>
2023-02-21 18:02:10 -0700

    built connection with database jwt, created userQueries and axios calls, added new routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const useQueries = require("../db/queries/users") ;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// sign up
+router.post('/signup', function (req, res, next) {
+  const userData = req.body;
+  console.log("sign up:", userData);
+  useQueries.createNewUser(userData)
+    .then(result => {
+      //generate a jwt token send it back to front end
+      res.json('user sucessfully signed up');
+    })
 });
 
 module.exports = router;
