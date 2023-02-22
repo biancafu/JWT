e2af5918dc650038a6a841917c8eb67cd912c8a0
Bianca Fu <0509biancafu@gmail.com>
2023-02-22 16:30:39 -0700

    set up jwt token in server, and pass to client side and store in local storage

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Alice', 'a123@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Kira', 'k123@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Alice', 'a123@gmail.com', 'password');
+INSERT INTO users (username, email, password) VALUES ('Kira', 'k123@gmail.com', 'password');
